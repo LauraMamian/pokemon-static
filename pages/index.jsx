99d4c9747f3e322fd264a6727e23fd5ebded3c39
LauraMamian <lauramamianc@unicauca.edu.co>
2023-01-13 11:58:06 -0500

    Pokémon cards

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,35 +1,18 @@
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { Grid } from "@nextui-org/react";
 
 import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
+import { PokemonCard } from "../components/pokemon";
 
 export default function HomePage({ pokemons }) {
-
-  console.log(pokemons);
 
   return (
     <>
       <Layout>
         <Grid.Container gap={2} justify='flex-start'>
           {
-            pokemons.map(({ id, name, image }) => (
-              <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-                <Card isHoverable isPressable>
-                  <Card.Body css={{ p: 1 }}>
-                    <Card.Image
-                      src={image}
-                      width='100%'
-                      height={140}
-                    />
-                  </Card.Body>
-                  <Card.Footer>
-                    <Row justify='space-between'>
-                      <Text transform="capitalize">{name}</Text>
-                      <Text> #{id}</Text>
-                    </Row>
-                  </Card.Footer>
-                </Card>
-              </Grid>
+            pokemons.map((pokemon) => (
+              <PokemonCard key={pokemon.id} pokemon={pokemon} />
             ))
           }
         </Grid.Container>
